13a89a20836149f024fdfed4c67db9a216944d04
jmkousickfarhad <jmkousickfarhad@gmail.com>
2021-04-26 01:26:48 +0600

    c2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+####################### INPUT/PRINT/CONCATANATION OF STRING/FORMATTING/VARIABLE TYPE
 # name = input('what is your name? ')
 # color = input('What is your Favourite color? ')
 # print(name + ' likes ' +color)
@@ -9,7 +10,7 @@
 # print(weight +'lbs = '+str(convertion)+'kg')
 # #cw 3 (input is always assigned as Sting in python)
 
-####################
+#################### MULTIPLE LINE UNDER ONE VARIABLE
 # message='''
 # hi kousick
 #
@@ -19,7 +20,7 @@
 # '''
 # print(message)
 
-############
+############ DELING WITH INDEX
 # name= 'Kousick Farhad'
 # print(name[0])#from left-K
 # print(name[-14])#from right-K
@@ -28,7 +29,7 @@
 # print(name[1:-1])#ousick Farha
 # print(name[:])#to copy and print whole name
 
-##################
+################## METHODS AND FUNCTION COMPAIRSM/ in OPERATOR
 # name='my name is Kousick Farhad'
 # print(len(name))
 # #here len or print are general purpose function means not only belong or useed in a specefic variable type or object..
@@ -49,4 +50,56 @@
 # #in operator (boolian)
 # print('Farhad' in name)# is Farhad exists in name variable?
 # print('JM' in name)
-##########################
+
+########################## ARITHMATIC OPERATORS
+#arithmatic operatprs are
+# + addition
+# - subtraction
+# / division with decimal point if exists
+# // only for quotient
+# % only for remainder
+# * for multiplication
+# ** for exponent
+
+########################## ARITHMATIC OPERATIOONS EXAMPLE
+#some arithmatic operations
+# x=3
+# x+=2 # same as x=x+2
+# print(x)
+
+# a=2
+# a **=3 #same as 2**3
+# print(a)
+
+# b=-2.76
+# print(round(b))#to round up
+# print(abs(b))#to get absulate value
+
+# ############### MATH MODULE
+# #we can import math module to make our mathmatical task easily.such as
+# import math
+# p=math.atan(math.isqrt(3))#other wise we have to write down code for trigonomatric function and so one.
+# print(p)
+# print(f'converted value is {math.degrees(float(p))} Degree')
+
+######################### IF/ELIF/ELSE WITH IN OPERATOR
+# #if elif else with in operator
+# print('''how is the weather codition?\nPress:\nH if it's "hot"\nC if it's "cold"\nR if it's "rainy"\nS if it's "shiny"''')
+# x=input('Enter the weather condition: ')
+# y=x.lower()
+# if 'c' in y:
+#     print('Wear worm cloth')
+# if 'h' in y:
+#     print('Drink water properly ')
+# if 'r' in y:
+#     print('Stay home or take rain coat while going outside')
+# if 's' in y:
+#     print('You can go to sea beach for enjoying the day')
+# else:
+#     print("You have enter wrong key" )
+#
+# print('Thank you')
+
+
+
+
