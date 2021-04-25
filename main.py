cfe72cb352c540b56c55935e99381efe6d521c6c
jmkousickfarhad <jmkousickfarhad@gmail.com>
2021-04-25 06:40:15 +0600

    c1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-message='''
-hi kousick 
-
-how are you
-
-it's not good :(
-'''
-print (message)
+# name = input('what is your name? ')
+# color = input('What is your Favourite color? ')
+# print(name + ' likes ' +color)
+# #cw 2
+#
+# weight = input('enter weight in Pound: ')
+# convertion = float(weight) * 0.453592
+# print(weight +'lbs = '+str(convertion)+'kg')
+# #cw 3 (input is always assigned as Sting in python)
+#
+# message='''
+# hi kousick
+#
+# how are you
+#
+# it's not good :(
+# '''
+# print(message)
